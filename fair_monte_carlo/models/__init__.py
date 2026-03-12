b3212591ac0a3b208ffa360b91f0cdd373c5aea8
pizzle67 <lindsey.pilver@gmail.com>
2026-03-12 11:51:36 -0700

    Delete fair_monte_carlo directory

--- fair_monte_carlo/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""FAIR model components."""
-
-from fair_monte_carlo.models.fair_model import FAIRModel
-from fair_monte_carlo.models.risk_scenario import RiskScenario
-
-__all__ = ["FAIRModel", "RiskScenario"]
--- fair_monte_carlo/reporting/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Reporting and visualization for FAIR analysis."""
-
-from fair_monte_carlo.reporting.report import RiskReport
-
-__all__ = ["RiskReport"]
